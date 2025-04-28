106ac0bfa316061c7b6636abaf82564d611f331f
Thakur-Harsh-07 <harshchauhan7081@gmail.com>
2025-04-28 21:04:47 +0530

    TYPEWRITER COMPONENT UPDATED

--- src/component/HomePage/Typeanimation.jsx
+++ src/component/HomePage/Typeanimation.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import Typewriter from 'typewriter-effect';
+import { Typewriter } from 'react-simple-typewriter';
 
 const TypeAnimation = () => {
     return (
         <div className="lg:text-5xl sm:text-2xl text-blue-500 font-bold">
             <Typewriter
-                options={{
-                    strings: [
-                        'Frontend Developer',
-                        'Backend Developer',
-                        'MERN Stack Developer',
-                        'Web Developer',
-                        'React Developer'
-                        
-                    ],
-                    autoStart: true,
-                    loop: true,
-                    deleteSpeed: 50,
-                    delay: 50
-                }}
+                words={[
+                    'Frontend Developer',
+                    'Backend Developer',
+                    'MERN Stack Developer',
+                    'Web Developer',
+                    'React Developer'
+                ]}
+                loop={true}
+                cursor
+                cursorStyle='|'
+                typeSpeed={80}
+                deleteSpeed={80}
+                delaySpeed={1000}
             />
         </div>
     );
