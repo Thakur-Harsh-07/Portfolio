569f8c444d0d30fbfa2c5d534037996e3cb0e6fb
Thakur-Harsh-07 <harshchauhan7081@gmail.com>
2025-06-21 19:23:18 +0530

    Bg Animation Added

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,26 @@ import Project from './component/Pages/Project'
 import Home from './component/Pages/Home'
 import Skills from './component/Pages/Skills'
 import Contact from './component/Pages/Contact'
-import {Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Footer from './component/Common/Footer'
-
 import Certificates from './component/Pages/Certificates'
+import Animation from './component/Common/Animation'
 
 function App() {
   return (
-    <div className='min-h-screen w-screen text-white flex flex-col'>
+    <div className='min-h-screen w-screen  flex flex-col'>
+      <Animation/>
       <Navbar />
+
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/skills" element={<Skills/>}/>
-        <Route path="/project" element={<Project/>}/>
-        <Route path='/certificates' element ={<Certificates/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/certificates" element={<Certificates />} />
       </Routes>
-    
-      <Footer/>
+
+      <Footer />
     </div>
   )
 }
---
+++ src/component/Common/Animation.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Animation.css'
+const Animation = () => {
+  return (
+    <div className="snowflakes" aria-hidden="true">
+    <div className="snowflake"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg "alt="snowflake" className='h-10 w-10'/></div>
+    <div className="snowflake"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg "alt="snowflake" className='h-10 w-10'/></div>
+    <div className="snowflake"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg"alt="snowflake" className='h-10 w-10'/></div>
+    <div className="snowflake"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg "alt="snowflake" className='h-10 w-10'/></div>
+    <div className="snowflake"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg "alt="snowflake" className='h-10 w-10'/></div>
+    <div className="snowflake"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg "alt="snowflake" className='h-10 w-10'/></div>
+    <div className="snowflake"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"alt="snowflake" className='h-10 w-10'/></div>
+    <div className="snowflake"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"alt="snowflake" className='h-10 w-10'/></div>
+   
+  </div>
+  )
+}
+export default Animation;
